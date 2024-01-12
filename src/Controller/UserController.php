5d5504721c630bfe3b77cb571ac2efa9ad773ec9
Adadie <junioradadie@gmail.com>
2024-01-12 02:40:47 +0200

    ft: map data on listing view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,28 +2,44 @@
 
 namespace App\Controller;
 
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+class User
+{
+    private static $lastId = 0;
+
+    public $id;
+    public $name;
+    public $marks;
+    public $email;
+    public $age;
+
+    public function __construct($name, $email, $age)
+    {
+        self::$lastId++;
+        $this->id = self::$lastId;
+        $this->name = $name;
+        $this->email = $email;
+        $this->age = $age;
+    }
+}
 class UserController extends AbstractController
 {
     #[Route('/users', name: 'app_user', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
-    }
 
-    #[Route('/users/{name}', name: 'app_user', methods: ['GET', 'HEAD'])]
-    public function single($name): JsonResponse
-    {
-        return $this->json([
-            'message' => "Welcome $name",
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $student = new User('john_doe', 'john.doe@example.com', 21);
+        $students[] = $student;
+
+        return $this->render('index.html.twig', ['students' => $students]);
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/UserController.php',
+        // ]);
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
